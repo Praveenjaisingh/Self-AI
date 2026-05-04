407021d3724bd3068c3b0962b099f8eae00bfdf6
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-05-04 12:08:53 +0530

    db issue fixed

--- src/Models/index.js
+++ src/Models/index.js
@@ -13,8 +13,13 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {dialect: 'postgres',dialectModule: pg,
-    logging: false,
+  sequelize = new Sequelize(process.env[config.use_env_variable], {dialect: 'postgres',dialectModule: pg,logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
 } else {
   sequelize = new Sequelize(config.database,config.username,config.password,
--- src/config/config.js
+++ src/config/config.js
@@ -20,22 +20,27 @@ module.exports = {
 
   },
 
+  // production: {
+
+  //   username: process.env.DB_USER,
+
+  //   password: process.env.DB_PASSWORD,
+
+  //   database: process.env.DB_NAME,
+
+  //   host: process.env.DB_HOST,
+
+  //   dialect: process.env.DB_DIALECT,
+
+  //   port: Number(process.env.DB_PORT),
+
+  //   logging: false
+
+  // }
+
   production: {
-
-    username: process.env.DB_USER,
-
-    password: process.env.DB_PASSWORD,
-
-    database: process.env.DB_NAME,
-
-    host: process.env.DB_HOST,
-
-    dialect: process.env.DB_DIALECT,
-
-    port: Number(process.env.DB_PORT),
-
-    logging: false
-
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   }
 
 };
